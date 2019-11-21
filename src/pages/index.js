f5c5c4e30e01fd0db087e8c14d3764e564666f9f
Juan Gabriel <tuxgabo@gmail.com>
2019-11-21 15:19:44 -0500

    some small changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <SEO title="Juan Gabriel Ramirez FrontEnd Developer" />
+    <h1>Juan Gabriel Ramirez FrontEnd Developer</h1>
+    <p>i build react sites and develop amazing experiences.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
