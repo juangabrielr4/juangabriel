34507ef65cb97658efb5477f9e2e802c664889d8
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-16 18:49:04 -0500

    some custom styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,19 @@ const IndexPage = () => (
     <SEO title="Juan Gabriel Ramirez FrontEnd Developer" />
     <h1>Juan Gabriel Ramirez FrontEnd Developer</h1>
     <p>i build react sites and develop amazing experiences.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <p>Passionate about developing amazing user experiences!</p>
+    <p>
+      Every position I have held has transformed my skill-set and goals towards
+      forward-thinking technology. I am obsessed with expanding my craft,
+      solving challenging problems and staying on the bleeding edge.
+    </p>
+    <div
+      style={{
+        borderRadius: `100%`,
+        maxWidth: `300px`,
+        marginBottom: `1.45rem`,
+      }}
+    >
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
