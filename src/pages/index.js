ab89aca3a9a48c4a7fd44fbcc92abf52ec37ce26
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-25 15:13:49 -0500

    better styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,29 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-        <Layout>
-        <SEO title="Juan Gabriel Ramirez FrontEnd Developer" />
-        <h1>Juan Gabriel Ramirez FrontEnd Developer</h1>
-        <div style={{display:`flex`}}>
+    <Layout>
+      <SEO title="Juan Gabriel Ramirez FrontEnd Developer" />
+      <h1>Juan Gabriel Ramirez FrontEnd Developer</h1>
+      <div style={{display:`flex`, alignItems:`center`}}>
         <p>i build react sites and develop amazing experiences.</p>
         <div style={{
-        borderRadius: `100%`,
-        overflow: `hidden`,
-        maxWidth: `300px`,
-        marginBottom: `1.45rem`,
-    }}
+            borderRadius: `100%`,
+            overflow: `hidden`,
+            width: `300px`,
+            marginBottom: `1.45rem`,
+        }}
         >
-        <Image />
+          <Image />
         </div>
-        <p>Passionate about developing amazing user experiences!</p>
-        </div>
-        <p>
+        <p style={{textAlign:`right`}}>Passionate about developing amazing user experiences!</p>
+      </div>
+      <p>
         Every position I have held has transformed my skill-set and goals towards
-    forward-thinking technology. I am obsessed with expanding my craft,
-    solving challenging problems and staying on the bleeding edge.
-        </p>
-        <Link to="/page-2/">Go to page 2</Link>
-        </Layout>
+        forward-thinking technology. I am obsessed with expanding my craft,
+        solving challenging problems and staying on the bleeding edge.
+      </p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
 )
 
 export default IndexPage
