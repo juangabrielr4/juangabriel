5f4c708bc261749e1d64e593c800e81648008049
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-29 09:33:27 -0500

    oceanic next colors in the website

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#1B2B34`,
       marginBottom: `1.45rem`,
     }}
   >
