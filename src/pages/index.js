55254a984aa8746af301f893b3f46c1267eb56ce
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-29 10:47:48 -0500

    formating with prettier and neoformat

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components"
 
 import { motion } from "framer-motion"
 const variants = {
@@ -13,49 +13,56 @@ const variants = {
 }
 
 const SoftAppear = ({ children }) => (
-  <motion.div initial="hidden" animate="visible" variants={variants} >
+  <motion.div initial="hidden" animate="visible" variants={variants}>
     {children}
   </motion.div>
 )
 // Create a <Title> react component that renders an <h1> which is
 // centered, palevioletred and sized at 1.5em
 const Section = styled.p`
-    font-size: 0.8em;
-    color: #99C794;
-    ${props => props.right && css`
-    text-align: right;
-`}
-`;
+  font-size: 0.8em;
+  color: #99c794;
+  ${props =>
+    props.right &&
+    css`
+      text-align: right;
+    `}
+`
 const IndexPage = () => (
-    <Layout>
-      <SEO title="Juan Gabriel Ramirez FrontEnd Developer" />
-      <h1 style={{color:`#5FB3B3`,fontSize:`21px`,textAlign:`right`}}>
+  <Layout>
+    <SEO title="Juan Gabriel Ramirez FrontEnd Developer" />
+    <h1 style={{ color: `#5FB3B3`, fontSize: `21px`, textAlign: `right` }}>
       Juan Gabriel Ramirez FrontEnd Developer
-      </h1>
-      <div style={{display:`flex`, alignItems:`center`}}>
-        <SoftAppear>
-          <Section>i build react sites and develop amazing experiences.</Section>
-        </SoftAppear>
-        <div style={{
-            borderRadius: `100%`,
-            overflow: `hidden`,
-            width: `300px`,
-            marginBottom: `1.45rem`,
+    </h1>
+    <div style={{ display: `flex`, alignItems: `center` }}>
+      <SoftAppear>
+        <Section>i build react sites and develop amazing experiences.</Section>
+      </SoftAppear>
+      <div
+        style={{
+          borderRadius: `100%`,
+          overflow: `hidden`,
+          width: `300px`,
+          marginBottom: `1.45rem`,
         }}
-        >
-          <Image />
-        </div>
-        <SoftAppear>
-          <Section right>Passionate about developing amazing user experiences!</Section>
-        </SoftAppear>
+      >
+        <Image />
       </div>
-      <p>
-        Every position I have held has transformed my skill-set and goals towards
-        forward-thinking technology. I am obsessed with expanding my craft,
-        solving challenging problems and staying on the bleeding edge.
-      </p>
-      <Link style={{color:`#FAC863`}} to="/page-2/">Go to page 2</Link>
-    </Layout>
+      <SoftAppear>
+        <Section right>
+          Passionate about developing amazing user experiences!
+        </Section>
+      </SoftAppear>
+    </div>
+    <p>
+      Every position I have held has transformed my skill-set and goals towards
+      forward-thinking technology. I am obsessed with expanding my craft,
+      solving challenging problems and staying on the bleeding edge.
+    </p>
+    <Link style={{ color: `#FAC863` }} to="/page-2/">
+      Go to page 2
+    </Link>
+  </Layout>
 )
 
 export default IndexPage
